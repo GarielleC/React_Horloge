5736e59a2c4e55747ab318317d8882059632669c
GarielleC <141339259+GarielleC@users.noreply.github.com>
2023-10-17 18:13:54 +0200

    exercice css

--- src/components/Horloge/Horloge.jsx
+++ src/components/Horloge/Horloge.jsx
@@ -19,20 +19,16 @@ function Clock() {
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
     const date = time.toLocaleDateString(); // permet d'obtenir la date complète
-    // Déterminez s'il fait jour ou nuit en fonction de l'heure actuelle 
-  const isDay = hours >= 6 && hours < 18;
+
 
     return (
         <div className='Num'>
-            <div className={`Num ${isDay ? 'day' : 'night'}`}>
-                <h1>Horloge</h1>
-                <p>{date}</p>
-                <p>
-                    {hours < 10 ? `0${hours}` : hours}:
-                    {minutes < 10 ? `0${minutes}` : minutes}:
-                    {seconds < 10 ? `0${seconds}` : seconds}
-                </p>
-            </div>
+            <h1>Horloge</h1>
+            <p>{date}</p>
+            <p className='Hours'>
+                {hours < 10 ? `0${hours}` : hours}</p>:
+            <p className='Minutes'>{minutes < 10 ? `0${minutes}` : minutes}</p>:
+            <p className='Seconds'>{seconds < 10 ? `0${seconds}` : seconds}</p>
         </div>
     );
 }
